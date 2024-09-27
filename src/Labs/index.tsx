224209ba6aa91dcd68df9fb1f8fbfcf090b3a437
shreyans <shreyansms@gmail.com>
2024-09-27 17:26:42 -0400

    fixed lab 1 routing

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,11 +1,13 @@
 import './labs.css';
-import HeadingsAndParagraphs from './headingsandpara';
-import Lists from './lists';
-import Forms from './forms';
-import Navbar from './navbar';
-import Table from './tables';
+import HeadingsAndParagraphs from './Lab 1/headingsandpara';
+import Lists from './Lab 1/lists';
+import Forms from './Lab 1/forms';
+import Navbar from './Lab 1/navbar';
+import Table from './Lab 1/tables';
+import Images from './Lab 1/images';
 import { HashRouter, Link, Route, Routes } from 'react-router-dom';
-import Images from './images';
+import LabsNavbar from './labsNavbar';
+import Lab1 from './Lab 1';
 
 const LabWork = () => {
     return (
@@ -24,18 +26,24 @@ const LabWork = () => {
             <h1>Lab Work</h1>
             <h1>Shreyans Mulkutkar</h1>
             <h4>CS5610 - 20596</h4>
-            <h2>Lab 1</h2>
+            {/* <h2>Lab 1</h2> */}
 
-            <Navbar />
+            <LabsNavbar />
 
             <Routes>
+                <Route path="/" element={<h3>Use the above navbar to view different components.</h3>} />
+                <Route path="lab1/*" element={<Lab1 />} />
+                {/* <Route path="Lab 2" element={<Lab2 />} /> */}
+            </Routes>
+
+            {/* <Routes>
                 <Route path="/" element={<h3>Use the above navbar to view different components.</h3>} />
                 <Route path="headingsandpara" element={<HeadingsAndParagraphs />} />
                 <Route path="lists" element={<Lists />} />
                 <Route path="tables" element={<Table />} />
                 <Route path="forms" element={<Forms />} />
                 <Route path="images" element={<Images />} />
-            </Routes>
+            </Routes> */}
         </div>
     );
 }
