93491e200acbd93d3c7ba9453a8af45e52edff4a
shreyans <shreyansms@gmail.com>
2024-10-06 17:25:00 -0400

    completed upto ds

--- src/Labs/Lab 3/index.tsx
+++ src/Labs/Lab 3/index.tsx
@@ -1,10 +1,25 @@
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
 import ArrowFunctions from "./ArrowFunctions";
 import BooleanVariables from "./BooleanVariables";
 import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import Destructing from "./Destructing";
+import DestructingImports from "./DestructingImports";
+import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+import ForLoops from "./ForLoops";
+import FunctionDestructing from "./FunctionDestructing";
+import House from "./House";
 import IfElse from "./IfElse";
+import JsonStringify from "./JsonStringify";
 import LegacyFunctions from "./LegacyFunctions";
+import MapFunction from "./MapFunction";
+import SimpleArrays from "./SimpleArrays";
+import Spreading from "./Spreading";
 import TemplateLiterals from "./TemplateLiterals";
 import Ternary from "./TernaryOperator";
+import TodoItem from "./TodoItem";
+import TodoList from "./TodoList";
 import VariablesAndConstants from "./VariablesAndConstants";
 import VariableTypes from "./VariableTypes";
 
@@ -28,6 +43,25 @@ export default function Lab3() {
       <LegacyFunctions />
       <ArrowFunctions />
       <TemplateLiterals />
+      <hr />
+
+      <h2>Data Structures</h2>
+      <hr />
+      <SimpleArrays />
+      <ArrayIndexAndLength />
+      <AddingAndRemovingToFromArrays />
+      <ForLoops />
+      <MapFunction />
+      <FindFunction />
+      <FindIndex />
+      <JsonStringify />
+      <House />
+      <TodoItem />
+      <TodoList />
+      <Spreading />
+      <Destructing />
+      <FunctionDestructing />
+      <DestructingImports />
     </div>
   );
 }
